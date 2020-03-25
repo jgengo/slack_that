c1bc1748424a9f32a0e3e1e3d349cde5505cf113
Titus <jordane@hive.fi>
2020-03-26 01:39:29 +0200

    feature: rate limiter to avoid to exceed the ratelimit if it get flooded.

--- slack.go
+++ slack.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"log"
 
 	"github.com/slack-go/slack"
+	"golang.org/x/time/rate"
 )
+
+var s = newSlackTask()
 
 // SlackCreate struct to store POST / body
 type SlackCreate struct {
@@ -14,6 +18,31 @@ type SlackCreate struct {
 	Text      string `json:"text"`       // ...
 	IconEmoji string `json:"icon_emoji"` // opt
 	Username  string `json:"username"`   // opt
+}
+
+// SlackTask ...
+type SlackTask struct {
+	limit *rate.Limiter
+}
+
+func newSlackTask() *SlackTask {
+	return &SlackTask{
+		limit: rate.NewLimiter(1, 1),
+	}
+}
+
+func (s *SlackTask) doSlackTask(body *SlackCreate) {
+	s.limit.Wait(context.Background())
+	resp, _, err := Gateway[body.Workspace].PostMessage(
+		body.Channel,
+		slack.MsgOptionText(body.Text, false),
+		slack.MsgOptionIconEmoji(body.IconEmoji),
+		slack.MsgOptionUsername(body.Username),
+	)
+	if err != nil {
+		log.Printf("error while trying to send %v:\n\t->%v", body, err)
+	}
+	log.Println(resp)
 }
 
 // ProcessCreate will threat the body and do the job!
@@ -32,12 +61,7 @@ func ProcessCreate(body *SlackCreate) error {
 		return errors.New("workspace doesn't exist")
 	}
 
-	Gateway[body.Workspace].PostMessage(
-		body.Channel,
-		slack.MsgOptionText(body.Text, false),
-		slack.MsgOptionIconEmoji(body.IconEmoji),
-		slack.MsgOptionUsername(body.Username),
-	)
+	go s.doSlackTask(body)
 
 	return nil
 }
